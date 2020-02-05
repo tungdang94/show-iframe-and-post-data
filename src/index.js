10e56bd0173cbaeec2df824a028611f97f0dcafd
Tung Dang <dtt.dangthanhtung@gmail.com>
2020-02-05 15:15:06 +0700

    Init source

--- src/index.js
+++ src/index.js
@@ -1,2 +1,31 @@
-console.log("Hello");
-console.log(1+5)
+$(document).ready(function() {
+  $("#btn-link-bank").click(function() {
+    var message = $("textarea#request").val();
+    var request = $.parseJSON(message);
+    console.log(request);
+
+    var form_data = new FormData();
+
+    for (var key in request) {
+      form_data.append(key, request[key]);
+    }
+
+    $.ajax({
+      type: "POST",
+      url: "https://cardtest.sacombank.com.vn:9448/checkout/stbCheckout",
+      processData: false,
+      contentType: false,
+      headers: {
+        Accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
+      },
+      data: form_data,
+      success: function(result) {
+        console.log(result);
+      },
+      error: function(err) {
+        console.log(err);
+      }
+    });
+  });
+});
